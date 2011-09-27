61bad1dc9f2826eb6bc2bc171dd2f858bba2ffd1
Eric Smith <eric@8thlight.com>
2011-09-26 21:29:36 -0500

    Initial commit

---
+++ src/PointerKata.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : PointerKata.cpp
+// Author      : Eric Smith
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is the C++ pointer kata." << endl; // prints !!!Hello World!!!
+	return 0;
+}
